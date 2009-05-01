18b0e5c4449f6dbbb01925eaef48ddc0b6cf0513
Jacob Burkhart <jacob@brontes3d.com>
2009-05-01 14:40:51 -0400

    using some simple lib thing instead of rbot

--- blimlimb.rb
+++ blimlimb.rb
@@ -1,43 +1,84 @@
-require 'rbot/rfc2812'
-require 'rbot/ircsocket'
-require 'rbot/timer'
-require 'rbot/message'
+# require 'rubygems'
+# require 'rbot/rfc2812'
+# require 'rbot/ircsocket'
+# require 'rbot/config'
+# require 'rbot/timer'
+# require 'rbot/message'
+# require 'rbot/irc'
+# require 'uri'
+require 'irc'
 
-HOST = 'irc.freenode.net'
-CHAN = '#stage'
+# HOST = 'irc.freenode.net'
+# CHAN = '#stagewtf'
+HOST = 'irc.mmm.com'
+CHAN = '#subtle_stage'
 FROM = 'blim.limb'
 
 def debug(message=nil)
-  print "DEBUG: #{message}\n" if message
+  print "=========DEBUG: #{message}\n" if message
 end
 
 class BotPlayer
-  attr_reader :nick, :socket, :client
-  def initialize(nick)
-    @socket = Irc::IrcSocket.new(HOST, 6667, false)
-    @client = Irc::IrcClient.new
-    @nick = nick
-    @client[:welcome] = proc do |data|
-      @socket.queue "JOIN #{CHAN}"
+  attr_reader :nick, :socket, :client, :people_to_thank, :people_to_talk_to
+  def initialize(nick, troupe)
+    @people_to_thank = []
+    @people_to_talk_to = []
+    # @socket = Irc::Socket.new([HOST], nil)
+    # @client = Irc::Client.new
+    @socket = IRC.new(HOST, 6667, nick, CHAN) do |who_said, said_what|
+      firstpart, message = said_what.split(":")
+      if firstpart == nick
+        #handle a message to me
+        unless troupe.my_names.include?(nick)
+          @people_to_talk_to << [who_said, message]
+        end
+      elsif said_what[nick]
+        unless troupe.my_names.include?(nick)
+          @people_to_thank << who_said
+        end
+        #handle a message that mentions me
+      end
+      # puts "running callback in #{self.inspect} --- #{callback}"
     end
+    @nick = nick
+    # @client[:welcome] = proc do |data|
+      # @socket.queue "JOIN #{CHAN}"
+    # end
   end
   def connect
-    @socket.connect
-    @socket.puts "NICK #{@nick}\nUSER #{@nick} 4 #{FROM} :blimLimb, of #camping"
-    @socket.puts "JOIN #{CHAN}"
-    Thread.start(self) do |bot|
-      while true
-        while bot.socket.connected?
-          if bot.socket.select
-            break unless reply = bot.socket.gets
-            bot.client.process reply
-          end
-        end
+    # irc = IRC.new('irc.mmm.com', 6667, 'Alt-255', '#stage')
+    @socket.connect()
+    Thread.new do
+      begin
+          @socket.main_loop()
+      rescue Interrupt
+      rescue Exception => detail
+          puts detail.message()
+          print detail.backtrace.join("\n")
+          retry
       end
-    end
+    end    
+    # @socket.connect
+    # @client.connect
+    # @socket.send :puts, "NICK #{@nick}\nUSER #{@nick} 4 #{FROM} :blimLimb, of #camping"
+    # @socket.send :puts, "JOIN #{CHAN}"
+    # while true
+    #   Thread.pass
+    # end
+    # Thread.start(self) do |bot|
+    #   while true
+    #     while bot.socket.connected?
+    #       if bot.socket.select
+    #         break unless reply = bot.socket.gets
+    #         bot.client.process reply
+    #       end
+    #     end
+    #   end
+    # end
   end
   def msg(type, where, message)
-    @socket.queue("#{type} #{where} :#{message}")
+    # debug("#{type} #{where} :#{message}")
+    @socket.send("#{type} #{where} :#{message}")
   end
   def say(message)
     msg("PRIVMSG", CHAN, message)
@@ -46,50 +87,85 @@ class BotPlayer
     msg("PRIVMSG", CHAN, "\001ACTION #{message}\001")
   end
   def renick(name)
+    debug("renick #{name}")
     @nick = name
-    @socket.queue("NICK #{@nick}")
+    @socket.send("NICK #{@nick}")
   end
   def quit(message)
-    @socket.puts "QUIT :#{message}"
-    @socket.flush
-    @socket.shutdown
+    debug("quit #{message}")
+    @socket.send "QUIT :#{message}"
+    # @socket.shutdown
   end
 end
 
 class BotTroupe
+  attr_accessor :actors, :my_names
   def initialize(script)
     @script = script
     @actors = {}
+    @my_names = []
   end
   def parse line
       case line.strip
+      #[t] teslyANDERSowens
+      #kid = t
+      #actor = teslyANDERSowens
+      #assigns t to 'teslyANDERSowens'
       when /^\[(\w+)\]\s+(.+?)$/
         kid, actor = $1, $2
-        @actors[kid] = BotPlayer.new(actor)
+        @actors[kid] = BotPlayer.new(actor, self)
         @actors[kid].connect
         puts "Join #{actor}"
+      #b= prin
+      #kid = b
+      #nick = prin
+      #renames b to 'prin'
       when /^(\w+)=\s*(.+?)$/
         kid, nick = $1, $2
         puts "#{@actors[kid].nick} is now named #{nick}"
         @actors[kid].renick(nick)
+      #t: hehe
+      #kid = t
+      #msg = hehe
+      #sends a message as 't'
       when /^(\w+):\s?(.+?)$/
         kid, msg = $1, $2
         @actors[kid].say(msg)
         puts "<#{@actors[kid].nick}> #{msg}"
+      #(t) picks up hanky
+      #kid = t
+      #msg = picks up hanky
+      #emotes message as 't'
       when /^\((\w+)\)\s+(.+?)$/
         kid, msg = $1, $2
         @actors[kid].action(msg)
         puts "** #{@actors[kid].nick} #{msg}"
+      #+10
+      #waits 10 seconds
       when /^\+\s*(\d+)/
         puts "Wait #{$1} secs"
         sleep $1.to_i
+      #* pick a volunteer
+      #parse standard in for stuff
+      #end it with a \
       when /^\*\s+(.+?)$/
         puts "--- okay, _why: your console - #$1 ---"
         while true
           puppet = $stdin.gets.strip
-          break if puppet =~ /^\*/
+          if puppet =~ /^\*/
+            #enter a * to go back to the script
+            break
+          end          
           parse puppet
         end 
+      when /^\?/
+        @actors.each do |a_name, bot|
+          puts "[#{a_name}] " + bot.nick
+          puts "people_to_thank: " + bot.people_to_thank.inspect
+          puts "people_to_talk_to: " + bot.people_to_talk_to.inspect
+        end
+      #-s away!!
+      #bot 's' exits (can't get the exit message to actually work)
       when /^\-(\w+)\s+(.+?)$/
         kid, msg = $1, $2
         @actors[kid].quit(msg)
@@ -97,6 +173,7 @@ class BotTroupe
       end
   rescue => e
       p e.message
+      puts e.backtrace.join("\n")
   end
   def act!
     IO.foreach(@script) do |line|
